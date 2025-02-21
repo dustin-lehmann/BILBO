e7712bc6d7f4ace45b27a72debc304c5cd33053c
Dustin Lehmann <dustin.lehmann@tu-berlin.de>
2025-02-21 19:06:08 +0100

    added big version of bilbo

---
+++ software/robot/firmware/stm32-mcu-projects/twipr_firmware_project_rev4/Debug/stm32_core_cpp_lib/ext/madgwick_ahrs/MadgwickAHRS (conflicted copy 2025-02-21 163201).d
@@ -0,0 +1,4 @@
+stm32_core_cpp_lib/ext/madgwick_ahrs/MadgwickAHRS.o: \
+ /Users/lehmann/work_dir/work_dir/projects/testbed/robots/BILBO/software/robot/firmware/libraries/stm32_core_cpp_lib/ext/madgwick_ahrs/MadgwickAHRS.cpp \
+ /Users/lehmann/work_dir/work_dir/projects/testbed/robots/BILBO/software/robot/firmware/libraries/stm32_core_cpp_lib/ext/madgwick_ahrs/MadgwickAHRS.h
+/Users/lehmann/work_dir/work_dir/projects/testbed/robots/BILBO/software/robot/firmware/libraries/stm32_core_cpp_lib/ext/madgwick_ahrs/MadgwickAHRS.h:
